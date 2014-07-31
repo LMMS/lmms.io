1681a54ff08aae45c1f64978e420f3b24bf4a094
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-07-30 23:34:11 -0400

    index.php content changes
    A lot of rewording, etc.  Please review.

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php include('header.php'); ?>
 <div class="page-header">
-	<h1>Welcome</h1>
+	<h3>Welcome to the home of the LMMS community</h3>
 </div>
 
 <!-- <div class="row"> -->
 	<div style="float:right;" class="col-sm-3">
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h3 class="panel-title">Downloads</h3>
+				<h3 class="panel-title">Download Now</h3>
 			</div>
 			<div style="max-height: 140px;" class="panel-body">
 <!-- Loads download links automatically from dl.php -->
@@ -18,21 +18,47 @@
 <!-- </div> -->
 
 
-
-<p>This is the home of the LMMS community.  LMMS is a free cross-platform software which allows you to produce music with your computer. This includes creating melodies and beats, synthesizing and mixing sounds and arranging samples. You can have fun with your MIDI keyboard and much more all in a user-friendly and modern interface. Furthermore LMMS comes with many ready-to-use instrument and effect plugins, presets and samples.</p>
+<h4>LMMS is a free cross-platform software which allows you to produce music with your computer.</h4>
+<p>Creating melodies and beats, synthesizing and mixing sounds, arranging samples and much more.<br>
+Playback instruments, samples and plugins with a typing or MIDI keyboard in a user-friendly and modern interface.<br>
+Bundled with many ready-to-use instrument and effect plugins, presets and samples.</p>
 <br><br>
 <div class="page-header">
 	<h1>Features</h1>
 </div>
-<a target="new" href="http://lmms.sourceforge.net/screenshots/1.0.0/lmms-1.0.0-3.png"><img class="img-thumbnail" alt="426x266" src="http://lmms.sourceforge.net/screenshots/1.0.0/lmms-1.0.0-3.png" style="float:right; width: 426px; height: 266px;"></a>
+<div style="float:right;"> 
+<a target="new" href="img/ss_proj.png"><img class="img-thumbnail" alt="350x169" src="img/th_ss_proj.png" style="float: top; width: 350px; height: 169px;"></a><br><br>
+<a target="new" href="img/ss_song.png"><img class="img-thumbnail" alt="350x169" src="img/th_ss_song.png" style="float: middle; width: 350px; height: 169px;"></a><br><br>
+<a target="new" href="img/ss_instr.png"><img class="img-thumbnail" alt="350x169" src="img/th_ss_instr.png" style="float: bottom; width: 350px; height: 169px;"></a>
+</div>
 <ul>
-	<li>Song-Editor for composing songs</li>
-	<li>A Beat+Bassline-Editor for creating beats and basslines</li>
-	<li>An easy-to-use Piano-Roll for editing patterns and melodies</li>
-	<li>An FX mixer with 64 FX channels and arbitrary number of effects allow unlimited mixing possibilities</li>
-	<li>Many powerful instrument and effect plugins out of the box</li>
+    <li>Compose music on Windows, Linux and Apple OS X</li>	
+    <li>Sequence, compose, mix and automate songs in one simple interface</li>
+	<li>Note playback via MIDI or typing keyboard</li>
+	<li>Consolidate instrument tracks using Beat+Bassline Editor</li>
+	<li>Fine tune patterns, notes, chords and melodies using Piano Roll Editor</li>
+	<li>FX mixer with unlimited FX channels:</li>
+		<ul>
+			<li>Drop-in LADSPA plug-in support</li>
+			<li>Drop-in VST ® effect plug-in support on (Linux and Windows)</li>
+			<li>Built-in compressor, limiter, delay, reverb, distortion, EQ, bass-enhancer</li>
+			<li>Bundled graphic and parametric equalizers</li>
+			<li>Built-in visualization/spectrum analyser</li>
+		</ul>
+	<li>Over 15 built-in instruments including:</li>
+		<ul>
+		    <li>Built-in 32-bit VST instrument support</li>
+			<li>Built-in 64-bit VST instrument support with 32-bit VST bridge (64-bit Windows)</li>
+			<li>Roland ® TB-303 style monophonic bass synthesizer</li>
+			<li>Embedded ZynAddSubFx:  Polyphonic, mutlitimbral, microtonal, multi-voice additive, subtractive and pad synthesis all in one powerful plugin</li>
+			<li>Native Commodore 64 ® SID microchip/instrument emulation</li>
+			<li>Native SoundFont ® support (SF2), the industry standard for high quality instrument patches and banks</li>
+			<li>Nintendo ®, GameBoy ® and game sound effect emulation</li>
+			<li>2 built-in oscillator-based synthesizers</li>
+			<li>2 built-in wavetable-based synthesizers</li>
+			<li>Gravis UltraSound ® GUS Patch support</li>
+		</ul>
 	<li>Full user-defined track-based automation and computer-controlled automation sources</li>
-	<li>Compatible with many standards such as SoundFont2, VST(i), LADSPA, GUS Patches, and MIDI</li>
 	<li>Import of MIDI files, Hydrogen project files and FL Studio ® project files</li>
 </ul>
 <br>
