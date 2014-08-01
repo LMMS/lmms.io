9638e9db6ac78609a8a498585647f388342ea4d5
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-01 16:27:25 -0400

    Downloads dropdown

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css" rel="stylesheet">
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
 
 		<!-- FIXME per Bootstrap, these should be placed at the end of the document (footer.php) so the pages load faster -->
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
@@ -28,20 +28,26 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="/index.php"><img class="logo-sm" src="img/logo_sm.png" />LMMS</a>
+					<a class="navbar-brand" href="/index.php"><img class="visible-lg logo-sm" style="float: left;" src="/img/logo_sm.png" />LMMS</a>
 				</div>
 
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
 						<?php
-						   @menu_item("Home", "/index.php");
-						   @menu_item("Download");
-						   @menu_item("Screenshots");
-						   @menu_item("Tracks");
-						   @menu_item("Documentation");
-						   @menu_item("Community");
-						   @menu_item("Share", "/lsp/");
+							@menu_item('Home', '/index.php');
+							@menu_item('Download', '/download.php', true);
+							echo '<ul class="dropdown-menu" role="menu">';
+								@menu_item('<span class="fa fa-download"></span> Download LMMS', '/download.php');
+								@menu_item('<span class="fa fa-music"></span> Download Sample Packs', '/samples.php');
+								@menu_item('<span class="fa fa-picture-o"></span> Download Artwork', '/artwork.php');
+							// Important! - Make sure to close the parent list item tag with "</li>"
+							echo '</li></ul>';
+							@menu_item('Screenshots');
+							@menu_item('Tracks');
+							@menu_item('Documentation');
+							@menu_item('Community');
+							@menu_item('Share', '/lsp/');
 						?>
 					</ul>
 				</div>
@@ -49,6 +55,7 @@
 		</nav>
 		<div class="container theme-showcase" role="main">
 <?php
+
 
 
 /*
@@ -71,7 +78,7 @@ function get_page_name() {
  * Creates a simple tag <li><a href="menu_item.php">Menu Item</a></li>
  * Taking into consideration the "active" status/style
  */
-function menu_item($text, $url) {
+function menu_item($text, $url, $dropdown) {
 	// Determine the "Active Tab
 	if ($text == get_page_name()) {
 		$active = ' class="active"';
@@ -82,8 +89,12 @@ function menu_item($text, $url) {
 	if (is_null($url)) {
 		$url = '/' . strtolower($text) . '.php';
 	}
-
-	echo '<li' . $active . '><a href="' . $url . '">' . $text . '</a></li>';
+	if ($dropdown) {
+		// Important - This leaves an open <li> tag.  Must be closed manually.
+		echo '<li' . $active . '><a href="' . $url . '" class="dropdown-toggle" data-toggle="dropdown">' . $text . ' <span class="caret"></span></a>';
+	} else {
+		echo '<li' . $active . '><a href="' . $url . '">' . $text . '</a></li>';
+	}
 }
 
 ?>
