446f7c23c6af91bfc990744a2a4b07767ce580da
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-01 16:26:01 -0400

    Fix typo

--- feed/issues.php
+++ feed/issues.php
@@ -21,9 +21,9 @@ $count = 0;
  * Echo our data to the page
  */
 foreach($obj as $item) {
-	echo '<a target="new" href="' .
+	echo '<a target="_blank" href="' .
 		$item->html_url . '"><span class="label label-info">#' . $item->number . '</span></a>&nbsp;';
-	echo '<a target="new" href="' . $item->html_url . '">' . $item->title. '</a><br>';
+	echo '<a target="_blank" href="' . $item->html_url . '">' . $item->title. '</a><br>';
 	if ($count++ == $max) {
 		break;
 	}
