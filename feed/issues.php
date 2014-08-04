e844eeb7bfbf7ed2fc0aa67094ced636844b26fc
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-04 17:44:04 -0400

    Add issue body feed in similar style as forums feed

--- feed/issues.php
+++ feed/issues.php
@@ -21,9 +21,18 @@ $count = 0;
  * Echo our data to the page
  */
 foreach($obj as $item) {
-	echo '<a target="_blank" href="' .
-		$item->html_url . '"><span class="label label-info">#' . $item->number . '</span></a>&nbsp;';
-	echo '<a target="_blank" href="' . $item->html_url . '">' . $item->title. '</a><br>';
+	echo '<div class="bs-callout bs-callout-info"><h5><strong><a target="_blank" href="' .
+		$item->html_url . '"><span class="fa fa-github"></span> GitHub #' . $item->number . ' &bull; ';
+	echo $item->title. '</a></strong></h5>';
+	$message = $item->body;
+	if (strlen($message) > 500) {
+		$message = substr($message, 0, 250) . '...<h4><a target="_blank" href="' . $item->html_url . '">...</a></h4>';
+	}
+	echo $message;
+	// Format and concat a pretty timestamp
+	echo '<small>Posted by: <a href="' . $item->user->html_url . '">' . $item->user->login . '</a> at ' . 
+		date("D, d M Y h:ia ", strtotime($item->created_at)) . '(GMT ' . sprintf('%+d', date('O')*1/100) . ')</small>';
+	echo '</div>';
 	if ($count++ == $max) {
 		break;
 	}
