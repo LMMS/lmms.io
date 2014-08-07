95a98c2748140ed7b9f151b7e18bf1d8ac293f9a
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-07 12:09:02 -0400

    Better facebook message parsing

--- feed/facebook.php
+++ feed/facebook.php
@@ -1,6 +1,7 @@
 <?php
-
+include_once('common.php');
 include_once('json_common.php');
+include_once('simple_html_dom.php');
 
 /*
  * Echo out the data
@@ -14,14 +15,15 @@ if (count($obj) <= 0 || !$obj->entries) {
 
 foreach ($obj->entries as $item) {
 		$title = $item->title;
+		
+		// Cross-linked posts seem to have an empty title.  This changes it to a generic title.
 		if (!$title || trim($title) == '') {
-			$title = 'No Subject';
+			$title = 'LMMS Announcement';
 		}
 		
 		echo '<div class="bs-callout bs-callout-info"><a target="_blank" href="' . $item->alternate . '"><h5><strong>';
 		echo '<span class="fa fa-facebook-square"></span> ' . $title . '</strong></h5></a>';
-		$message = $item->content;
-		$message = str_replace('href="/LMMSchallenge/', 'href="http://www.facebook.com/LMMSchallenge/', $message);
+		$message = cleanse_urls($item->content, $item->alternate);
 
 		echo $message;
 		// Format and concat a pretty timestamp
@@ -30,4 +32,55 @@ foreach ($obj->entries as $item) {
 		echo '</div><br>';
 }
 
+/*
+ * Fixes facebook JSON feed nuances such as:
+ * 	- Relative linking (i.e. href="/makefreemusic")
+ *  - URL redirecting (i.e. ...l.php?u=http://my_real_link)
+ *  - Render cross-posted thumbnails (i.e. <a src="https://fbcdn-profile-a.akamaihd.net...>)
+ */
+function cleanse_urls($str, $article_url) {
+	$html = str_get_html($str);
+	foreach($html->find('a') as $element) {
+		// Fix cross posts, especially those from LMMSChallenge facebook
+		if (!$element->find('img') && is_image($element)) {
+			$element->innertext = '<img src="' . $element->href . '"/>';
+			$element->href = $article_url;
+		}
+		// Fix unnecessary facebook redirects
+		$pos = strpos(strtolower($element->href), '.php?u=http%3a%2f%2f') + strpos(strtolower($element->href), '.php?u=https%3a%2f%2f');
+		if ($pos) {
+			// Remove js callbacks
+			$element->onmouseover = '';
+			$element->onclick = '';
+			// Strip out the facebook params
+			$href = explode('&', $element->href)[0];
+			// Isolate and decode the proper url
+			$element->href = urldecode(substr($href, $pos+7));
+		}
+		// Fix relative facebook URLs
+		if (str_startswith($element->href, '/')) {
+			$element->href = 'https://www.facebook.com' . $element->href;
+		}
+		
+	}
+	return $html->save();
+}
+
+/*
+ * Checks an href URL for most well-known image formats
+ */
+function is_image($a_tag) {
+	$image_exts = explode(',', 'png,jpg,jpeg,gif,bmp,tif,tiff,svg');
+	foreach($image_exts as $ext) {
+		if (str_endswith($a_tag->href, '.' . trim($ext), true)) {
+			return true;
+		}
+	}
+	return false;
+}
+
 ?>
+<script>
+// Facebook JavaScript stub to prevent lingering onmouseover and onclick page errors
+var LinkshimAsyncLink = { swap:function(caller, url) {} };
+</script>
