615313c2c53654db7f7c1f81bc99b75eae041d0d
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-08 09:02:32 -0400

    Revert "New Linux download page"

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css" rel="stylesheet">
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-		<link href="//lukas-w.github.com/font-linux/assets/font-linux.css" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="/css/style.css">
 
 		<!-- FIXME per Bootstrap, these should be placed at the end of the document (footer.php) so the pages load faster -->
