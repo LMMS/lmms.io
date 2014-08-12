ff73cb475ffabcbaf2aa32e15c2745d5a55eb7f2
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-12 14:29:31 -0400

    Move JavaScript to end per jquery/boostrap recommendation

--- footer.php
+++ footer.php
@@ -2,4 +2,6 @@
 <br><br><br><br><br>
 </div>
 </body>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </html>
--- header.php
+++ header.php
@@ -12,10 +12,6 @@
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href="//lukas-w.github.com/font-linux/assets/font-linux.css" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="/css/style.css">
-
-		<!-- FIXME per Bootstrap, these should be placed at the end of the document (footer.php) so the pages load faster -->
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
 
 	<body role="document">
