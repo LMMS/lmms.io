bedfce1d65df9ea0bf7a4e81db75ef82d152075e
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-12 15:18:51 -0400

    Revert JavaScript move

--- header.php
+++ header.php
@@ -11,7 +11,10 @@
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css" rel="stylesheet">
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href="//lukas-w.github.com/font-linux/assets/font-linux.css" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="/css/style.css">
+		<link href="/css/style.css" rel="stylesheet">
+		
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
 
 	<body role="document">
