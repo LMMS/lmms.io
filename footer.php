bcd0b5fa8a052e813c3fbd5ed5eddfb555a4f8ef
Lukas W <lukaswhl@gmail.com>
2014-08-14 13:35:32 +0200

    Add social network links to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
 <?php ?>
 </div><br>
-<div class="row text-center"><a href="http://www.networkredux.com/"><img src="/img/network_redux.png"></a></div>
+<div class="text-center">
+	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS"><span class="fa fa-github"></span> GitHub</a>
+	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS"><span class="fa fa-facebook"></span> Facebook</a>
+	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS"><span class="fa fa-google-plus"></span> Google+</a>
+	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS"><span class="fa fa-soundcloud"></span> Soundcloud</a>
+</div>
+<div class="text-center"><a href="http://www.networkredux.com/"><img src="/img/network_redux.png"></a></div>
 </body>
 </html>
