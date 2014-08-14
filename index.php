356ead3208e7dbd9544eedf1841f7bfcee05c01c
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-14 00:38:41 -0400

    More front page styling

--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php include_once('header.php'); ?>
 <?php include_once('feed/releases.php'); ?>
-
-<!-- <div class="row">
-	<div style="float:right;" class="visible-lg">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h3 class="panel-title">Download Now</h3>
-			</div>
-			<div style="overflow-y:visible;" class="panel-body">
-<!-- Loads download links automatically from feed/releases.php
-<?php //get_releases(); ?>
-			</div>
-		</div>
-	</div>
-<!-- </div> -->
 <div class="jumbotron jumbo">
 <div class="container">
-<div id="reflect-black" class="image-block visible-lg" style="float:right;">
-        <a target="_blank" href="img/ss_proj.png"><img src="img/th_ss_proj.png" alt="" /></a>
-        <div class="reflection">
-            <img src="img/th_ss_proj.png" alt="" />
-            <div class="overlay"></div>
-        </div>
- </div>
 
-<!-- <a class="visible-lg" style="float: right;" target="_blank" href="img/ss_proj.png"><img src="img/jumbotron_image.png"></a> -->
+<?php make_reflection('img/ss_proj.png', NULL, "black", "pull-right visible-lg"); ?>
+
 <h1 class="jumbo">Let's make music :)</h1>
 <p class="jumbo">with a free, cross-platform tool for your computer.</p><br>
 <a class="btn btn-primary btn-lg" href="https://www.youtube.com/watch?v=uWPfIIaAHQg" target="_blank" role="button">See how »</a>&nbsp;
@@ -38,17 +18,17 @@
 </div>
 <div style="text-align: center;" class="row">
 	<div class="col-lg-4">
-		<span class="fa-column fa fa-bar-chart-o"></span>
+		<span class="fa-xl fa fa-bar-chart-o"></span>
 		<h4>Produce music with your computer</h4>
 		<p>by creating melodies and beats, synthesizing and mixing sounds, arranging samples and much more.</p>
 	</div>
 	<div class="col-lg-4">
-		<span class="fa-column fa fa-keyboard-o"></span>
+		<span class="fa-xl fa fa-keyboard-o"></span>
 		<h4>Playback instruments, samples and plugins</h4>
 		<p>with a typing or MIDI keyboard in a user-friendly and modern interface.</p>
 	</div>
 	<div class="col-lg-4">
-		<span class="fa-column fa fa-puzzle-piece"></span>
+		<span class="fa-xl fa fa-puzzle-piece"></span>
 		<h4>Bundled with ready-to-use stuff</h4>
 		<p>from an assortment of instrument and effect plugins, presets and samples to VST and SoundFont support.</p>
 	</div>
@@ -57,44 +37,137 @@
 
 <p style="text-align: right;"><a class="label label-success hidden-lg" href="download/"><span class="glyphicon glyphicon-arrow-right"></span>&nbsp;Download Now</a></p>
 <br><br><br><br>
+
 <div style="text-align: center;" class="page-header">
-	<hr><h1>Features, features, features</h1>
+	<hr><h1>Made by musicians. Made for musicians.</h1>
 </div>
-<div style="float:right;">
-<a target="_blank" href="img/ss_proj.png"><img class="img-thumbnail visible-lg" alt="350x169" src="img/th_ss_proj.png" style="float: top; width: 350px; height: 169px;"></a><br><br>
-<a target="_blank" href="img/ss_song.png"><img class="img-thumbnail visible-lg" alt="350x169" src="img/th_ss_song.png" style="float: middle; width: 350px; height: 169px;"></a><br><br>
-<a target="_blank" href="img/ss_instr.png"><img class="img-thumbnail visible-lg" alt="350x169" src="img/th_ss_instr.png" style="float: bottom; width: 350px; height: 169px;"></a>
+
+<div style="text-align: center;" class="row">
+	<div>
+		<a href="/download/#windows" class="btn btn-default"><span class="fa-lg fa fa-windows"></span></a>&nbsp;
+		<a href="/download/#linux" class="btn btn-default"><span class="fa-lg fa fa-linux"></span></a>&nbsp;
+		<a href="/download/#mac" class="btn btn-default"><span class="fa-lg fa fa-apple"></span></a>&nbsp;
+		<h4>Take a pick</h4>
+		<p>for Windows, Linux or Apple OS X</p>
+	</div>
 </div>
-<ul>
-	<li>Compose music on Windows, Linux and Apple OS&nbsp;X</li>
-	<li>Sequence, compose, mix and automate songs in one simple interface</li>
-	<li>Note playback via MIDI or typing keyboard</li>
-	<li>Consolidate instrument tracks using Beat+Bassline Editor</li>
-	<li>Fine tune patterns, notes, chords and melodies using Piano Roll Editor</li>
-	<li>FX mixer with unlimited FX channels:</li>
+
+<br><br>
+<div style="text-align: center;" class="page-header">
+	<hr><h1>Houston, we have a problem</h1>
+	<h4>but don't worry, we've got you covered.</h4>
+</div>
+
+<div style="text-align: center;" class="row">
+	<div>
+		<a href="/community/forum/" class="btn btn-default"><span class="fa-lg fa fa-comments"></span></a>&nbsp;
+		<a href="http://facebook.com/makefreemusic" target="_blank" class="btn btn-default"><span class="fa-lg fa fa-facebook"></span></a>&nbsp;
+		<a href="https://plus.google.com/u/0/113001340835122723950/posts" target="_blank" class="btn btn-default"><span class="fa-lg fa fa-google-plus"></span></a>&nbsp;
+		<a href="http://github.com/lmms/lmms" target="_blank" class="btn btn-default"><span class="fa-lg fa fa-github"></span></a>
+		<h4>Ask your questions here</h4>
+		<p>and answer a few questions while you're waiting.</p>
+	</div>
+</div>
+
+<br><br>
+<div style="text-align: center;" class="page-header">
+	<hr><h1>Sharing means caring.</h1>
+</div>
+
+<div style="text-align: center;" class="row">
+	<div>
+		<a href="https://soundcloud.com/groups/linux-multimedia-studio" target="_blank" class="btn btn-default"><span class="fa-lg fa fa-soundcloud"></span></a>&nbsp;
+		<a href="/lsp/" class="btn btn-default"><span class="fa-lg fa fa-volume-down">	LSP</span></a>&nbsp;
+		<h4>Don't be modest,</h4>
+		<p>show us your stuff.</p>
+	</div>
+</div>
+<br><br>
+<div style="text-align: center; overflow: hidden;" class="row visible-lg">
+<div style="text-align: center;" class="page-header">
+	<hr><h1>Take a peek</h1>
+</div>
+	<div class="col-lg-4">
+		<h4>Overall Project</h4>
+		<?php make_reflection('img/ss_proj.png', NULL, "white"); ?>
+	</div>
+	<div class="col-lg-4">
+		<h4>Song Editor</h4>
+		<?php make_reflection('img/ss_song.png', NULL, "white"); ?>
+	</div>
+	<div class="col-lg-4">
+		<h4>Native Instruments</h4>
+		<?php make_reflection('img/ss_instr.png', NULL, "white"); ?>
+	</div>
+</div>
+	<p class="visible-lg" style="text-align:center;">More screenshots can be found <a href="/screenshots/">here</a>.</p>
+<br><br>
+
+<div>
+<div style="text-align: center;" class="page-header">
+	<hr><h1>Featuring</h1>
+</div>
+	<div class="col-lg-4">
+		<center><span class="fa-lg fa fa-check-square-o"></span>
+		<h2>Functionality</h2></center>
 		<ul>
-			<li>Drop-in LADSPA plug-in support</li>
-			<li>Drop-in VST ® effect plug-in support on (Linux and Windows)</li>
-			<li>Built-in compressor, limiter, delay, reverb, distortion, EQ, bass-enhancer</li>
-			<li>Bundled graphic and parametric equalizers</li>
-			<li>Built-in visualization/spectrum analyser</li>
+		<li>Compose music on Windows, Linux and Apple OS&nbsp;X</li>
+		<li>Sequence, compose, mix and automate songs in one simple interface</li>
+		<li>Note playback via MIDI or typing keyboard</li>
+		<li>Consolidate instrument tracks using Beat+Bassline Editor</li>
+		<li>Fine tune patterns, notes, chords and melodies using Piano Roll Editor</li>
+		<li>Full user-defined track-based automation and computer-controlled automation sources</li>
+		<li>Import of MIDI files, Hydrogen project files and FL Studio ® project files</li>
 		</ul>
-	<li>Over 15 built-in instruments including:</li>
+	</div>
+	<div class="col-lg-4">
+		<center><span class="fa-lg fa fa-bar-chart-o"></span>
+		<h2>Effects Mixing</h2></center>
 		<ul>
-			<li>Built-in 32-bit VST instrument support</li>
-			<li>Built-in 64-bit VST instrument support with 32-bit VST bridge (64-bit Windows)</li>
-			<li>Roland ® TB-303 style monophonic bass synthesizer</li>
-			<li>Embedded ZynAddSubFx:  Polyphonic, mutlitimbral, microtonal, multi-voice additive, subtractive and pad synthesis all in one powerful plugin</li>
-			<li>Native Commodore 64 ® SID microchip/instrument emulation</li>
-			<li>Native SoundFont ® support (SF2), the industry standard for high quality instrument patches and banks</li>
-			<li>Nintendo ®, GameBoy ® and game sound effect emulation</li>
-			<li>2 built-in oscillator-based synthesizers</li>
-			<li>2 built-in wavetable-based synthesizers</li>
-			<li>Gravis UltraSound ® GUS Patch support</li>
+		<li>Drop-in LADSPA plug-in support</li>
+		<li>Drop-in VST ® effect plug-in support on (Linux and Windows)</li>
+		<li>Built-in compressor, limiter, delay, reverb, distortion, EQ, bass-enhancer</li>
+		<li>Bundled graphic and parametric equalizers</li>
+		<li>Built-in visualization/spectrum analyser</li>
 		</ul>
-	<li>Full user-defined track-based automation and computer-controlled automation sources</li>
-	<li>Import of MIDI files, Hydrogen project files and FL Studio ® project files</li>
-</ul>
+	</div>
+	<div class="col-lg-4">
+		<center><span class="fa-lg fa fa-music"></span>
+		<h2>Musical Instruments</h2></center>
+		<ul>
+		<li>Built-in 32-bit VST instrument support</li>
+		<li>Built-in 64-bit VST instrument support with 32-bit VST bridge (64-bit Windows)</li>
+		<li>Roland ® TB-303 style monophonic bass synthesizer</li>
+		<li>Embedded ZynAddSubFx:  Polyphonic, mutlitimbral, microtonal, multi-voice additive, subtractive and pad synthesis all in one powerful plugin</li>
+		<li>Native Commodore 64 ® SID microchip/instrument emulation</li>
+		<li>Native SoundFont ® support (SF2), the industry standard for high quality instrument patches and banks</li>
+		<li>Nintendo ®, GameBoy ® and game sound effect emulation</li>
+		<li>2 built-in oscillator-based synthesizers</li>
+		<li>2 built-in wavetable-based synthesizers</li>
+		<li>Gravis UltraSound ® GUS Patch support</li>
+		</ul>
+	</div>
+</div>
+
 <br>
 
 <?php include('footer.php'); ?>
+<?php 
+
+function make_reflection($image_path, $thumbnail_path = NULL, $blackorwhite = 'black', $class = '') {
+	// If no thumbnail is supplied, try 'th_' . $image_path
+	if (!$thumbnail_path) {
+		$pieces = explode('/', $image_path);
+		$pieces[count($pieces) -1] = 'th_' . $pieces[count($pieces) -1];
+		$thumbnail_path = implode('/', $pieces);
+	}
+	echo '<div id="reflect-' . $blackorwhite . '" class="image-block ' . $class . '">';
+	echo '<a target="_blank" href="' . $image_path . '"><img src="' . $thumbnail_path . '" alt="" /></a>';
+	echo '<div class="reflection">';
+	echo '	<img src="' . $thumbnail_path . '" alt="" />';
+	echo '	<div class="overlay"></div>';
+	echo '</div>';
+	echo '</div>';
+}
+
+?>
