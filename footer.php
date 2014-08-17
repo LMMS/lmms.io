dd04fcf4cf3f411a33e2878817662a099d86c9ff
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-17 15:23:38 -0400

    Fix incorrect community footer links

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 </div><br>
 <div class="text-center">
 	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS"><span class="fa fa-github"></span> GitHub</a>
-	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS"><span class="fa fa-facebook"></span> Facebook</a>
-	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS"><span class="fa fa-google-plus"></span> Google+</a>
-	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS"><span class="fa fa-soundcloud"></span> Soundcloud</a>
+	<a class="btn btn-adefault btn-xs" href="http://facebook.com/makefreemusic"><span class="fa fa-facebook"></span> Facebook</a>
+	<a class="btn btn-adefault btn-xs" href="https://plus.google.com/113001340835122723950"><span class="fa fa-google-plus"></span> Google+</a>
+	<a class="btn btn-adefault btn-xs" href="http://soundcloud.com/groups/linux-multimedia-studio" rel="publisher"><span class="fa fa-soundcloud"></span> Soundcloud</a>
 </div>
 <div class="text-center"><a href="http://www.networkredux.com/"><img src="/img/network_redux.png"></a></div>
 </body>
