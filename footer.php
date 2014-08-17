1a5ccd5cd062fac92192cc9f06d209b47a3d29d3
Umcaruje <uros.maravic@gmail.com>
2014-08-18 00:49:36 +0200

    Nicer NetworkRedux banner,  Footer buttons open in new window

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <?php ?>
 </div><br>
 <div class="text-center">
-	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS"><span class="fa fa-github"></span> GitHub</a>
-	<a class="btn btn-adefault btn-xs" href="http://facebook.com/makefreemusic"><span class="fa fa-facebook"></span> Facebook</a>
-	<a class="btn btn-adefault btn-xs" href="https://plus.google.com/113001340835122723950"><span class="fa fa-google-plus"></span> Google+</a>
-	<a class="btn btn-adefault btn-xs" href="http://soundcloud.com/groups/linux-multimedia-studio" rel="publisher"><span class="fa fa-soundcloud"></span> Soundcloud</a>
+	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS" target="_blank"><span class="fa fa-github"></span> GitHub</a>
+	<a class="btn btn-adefault btn-xs" href="http://facebook.com/makefreemusic" target="_blank"><span class="fa fa-facebook"></span> Facebook</a>
+	<a class="btn btn-adefault btn-xs" href="https://plus.google.com/113001340835122723950" target="_blank"><span class="fa fa-google-plus"></span> Google+</a>
+	<a class="btn btn-adefault btn-xs" href="http://soundcloud.com/groups/linux-multimedia-studio target="_blank"" rel="publisher"><span class="fa fa-soundcloud"></span> Soundcloud</a>
 </div>
-<div class="text-center"><a href="http://www.networkredux.com/"><img src="/img/network_redux.png"></a></div>
+<div class="text-center"><a href="http://www.networkredux.com/" target="_blank"><img src="/img/network_redux.png"></a></div>
 </body>
 </html>
