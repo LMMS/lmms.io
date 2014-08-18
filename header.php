837b4a57140264c07e00ae5cd56b9b0f51f8a7f2
Umcaruje <uros.maravic@gmail.com>
2014-08-18 23:28:09 +0200

    Lightbox: Download button

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="/js/lightbox.min.js"></script>
+		<script type="text/javascript" src="/js/lightbox.js"></script>
 	</head>
 
 	<body role="document">
