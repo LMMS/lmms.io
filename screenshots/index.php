74cdca5fb1089f4d519d4f3cc62b6906a5b82b92
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-08-21 09:59:48 -0400

    Screenshots enhancements
    - Move screenshots to dedicated directory
    - Generate thumbnails and descriptions dynamically from folder contents

--- screenshots/index.php
+++ screenshots/index.php
@@ -6,32 +6,56 @@ include_once('../utils.php');
 	<h1>Screenshots</h1>
 </div>
 <div class="row center overflow-hidden">
-	<div class="col-lg-4">
-		<h4>Song editor</h4>
-		<?php make_reflection('/img/ss_song_editor.png', NULL, "white"); ?>
-	</div>
-	<div class="col-lg-4">
-		<h4>Automation editor</h4>
-		<?php make_reflection('/img/ss_automation.png', NULL, "white"); ?>
-	</div>
-	<div class="col-lg-4">
-		<h4>B&amp;B editor and the FX-mixer</h4>
-		<?php make_reflection('/img/ss_bb_mixer.png', NULL, "white"); ?>
-	</div>
-	<div class="col-lg-4">
-		<h4>Piano Roll editor</h4>
-		<?php make_reflection('/img/ss_piano_roll.png', NULL, "white"); ?>
-	</div>
-	<div class="col-lg-4">
-		<h4>Native instruments</h4>
-		<?php make_reflection('/img/ss_plugins.png', NULL, "white"); ?>
-	</div>
-	<div class="col-lg-4">
-		<h4>Two VSTi running via Vestige</h4>
-		<?php make_reflection('/img/ss_vst.png', NULL, "white"); ?>
-	</div>
+<?php
+	$ss_dir = '../img/screenshots/';
+	$files = scandir($ss_dir);
+	foreach($files as $file) {
+		if (str_startswith($file, 'ss_', true) && str_endswith($file, '.png', true)) {
+			echo '<div class="col-lg-4">';
+			echo '<h4>' . humanize_title($file) . '</h4>';
+			make_reflection($ss_dir . $file, NULL, "white");
+			echo '</div>';
+		}
+	}
+?>
 </div>
 
+<?php 
 
-<?php ?>
+/*
+ * Creates an english-readable title from a file name
+ */
+function humanize_title($filename) {
+	$replacement = array(
+		'ss' => '',
+		'bb' => 'B&amp;B Editor',
+		'mixer' => 'FX Mixer',
+		'roll' => 'Roll Editor',
+		'plugins' => 'Native Instruments',
+		'automation' => 'Automation Editor',
+		'vst' => 'VSTi Running via Vestige'
+	);
+	
+	$title_split = explode('_', $filename);
+
+	$found = false;
+	foreach($title_split as &$item) {
+		// Substitute array reference with the text above
+		if (str_contains($item, '.png', false)) {
+			$item = str_replace('.png', '', $item);
+		}
+		
+		if (array_key_exists($item, $replacement)) {
+			$temp = $replacement[$item];
+			$item = ($found ? ', ' : ' ') . $temp;
+			$found = trim($temp) != '' ? true : false;
+		} else {
+			$item = ' ' . ucfirst($item);
+		}
+	}
+	
+	return trim(implode('', $title_split));
+}
+?>
+
 <?php include('../footer.php'); ?>
