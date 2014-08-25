ad18f260f306bdbff8254b4d63e9daa4cf5ca3cd
Umcaruje <uros.maravic@gmail.com>
2014-08-25 21:33:44 +0200

    Fix a typo in footer links

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 	<a class="btn btn-adefault btn-xs" href="//github.com/LMMS" target="_blank"><span class="fa fa-github"></span> GitHub</a>
 	<a class="btn btn-adefault btn-xs" href="http://facebook.com/makefreemusic" target="_blank"><span class="fa fa-facebook"></span> Facebook</a>
 	<a class="btn btn-adefault btn-xs" href="https://plus.google.com/113001340835122723950" target="_blank"><span class="fa fa-google-plus"></span> Google+</a>
-	<a class="btn btn-adefault btn-xs" href="http://soundcloud.com/groups/linux-multimedia-studio target="_blank"" rel="publisher"><span class="fa fa-soundcloud"></span> Soundcloud</a>
+	<a class="btn btn-adefault btn-xs" href="http://soundcloud.com/groups/linux-multimedia-studio" target="_blank" rel="publisher"><span class="fa fa-soundcloud"></span> Soundcloud</a>
 </div>
 <div class="text-center"><a href="http://www.networkredux.com/" target="_blank"><img src="/img/network_redux.png"></a></div>
 </body>
