52f0a1998554c40f4daacb05af5ee8c6edfe37c9
Lukas W <lukaswhl@gmail.com>
2014-10-02 11:26:25 +0200

    Indentation fixes

--- public/index.php
+++ public/index.php
@@ -3,148 +3,148 @@ include_once('header.php');
 include_once('utils.php');
 ?>
 <div class="jumbotron jumbo">
-<div class="container">
+	<div class="container">
 
-<?php make_reflection('img/ss_proj.png', NULL, "black", "pull-right visible-lg"); ?>
+		<?php make_reflection('img/ss_proj.png', NULL, "black", "pull-right visible-lg"); ?>
 
-<h1 class="jumbo">Let's make music :)</h1>
-<p class="jumbo">with a free, cross-platform tool for your computer.</p><br>
-<a class="btn btn-primary btn-lg" href="https://www.youtube.com/watch?v=uWPfIIaAHQg" target="_blank" role="button">See how »</a>&nbsp;
-<a class="btn btn-primary btn-lg" href="/download/" role="button">Download now »</a>&nbsp;
-<a class="btn btn-primary btn-lg" href="/community/" role="button">Get involved »</a>
-</div>
+		<h1 class="jumbo">Let's make music :)</h1>
+		<p class="jumbo">with a free, cross-platform tool for your computer.</p><br>
+		<a class="btn btn-primary btn-lg" href="https://www.youtube.com/watch?v=uWPfIIaAHQg" target="_blank" role="button">See how »</a>&nbsp;
+		<a class="btn btn-primary btn-lg" href="/download/" role="button">Download now »</a>&nbsp;
+		<a class="btn btn-primary btn-lg" href="/community/" role="button">Get involved »</a>
+	</div>
 </div>
 <br><br><br><br>
 
 <div class="text-center">
-<div class="page-header">
-	<hr><h1>Making beats has never been easier.</h1>
-</div>
-<div class="row">
-	<div class="col-lg-4">
-		<span class="fa-8x fa fa-bar-chart-o"></span>
-		<h4>Produce music with your computer</h4>
-		<p>by creating melodies and beats, synthesizing and mixing sounds, arranging samples and much more.</p>
+	<div class="page-header">
+		<hr><h1>Making beats has never been easier.</h1>
 	</div>
-	<div class="col-lg-4">
-		<span class="fa-8x fa fa-keyboard-o"></span>
-		<h4>Playback instruments, samples and plugins</h4>
-		<p>with a typing or MIDI keyboard in a user-friendly and modern interface.</p>
+	<div class="row">
+		<div class="col-lg-4">
+			<span class="fa-8x fa fa-bar-chart-o"></span>
+			<h4>Produce music with your computer</h4>
+			<p>by creating melodies and beats, synthesizing and mixing sounds, arranging samples and much more.</p>
+		</div>
+		<div class="col-lg-4">
+			<span class="fa-8x fa fa-keyboard-o"></span>
+			<h4>Playback instruments, samples and plugins</h4>
+			<p>with a typing or MIDI keyboard in a user-friendly and modern interface.</p>
+		</div>
+		<div class="col-lg-4">
+			<span class="fa-8x fa fa-puzzle-piece"></span>
+			<h4>Bundled with ready-to-use stuff</h4>
+			<p>from an assortment of instrument and effect plugins, presets and samples to VST and SoundFont support.</p>
+		</div>
 	</div>
-	<div class="col-lg-4">
-		<span class="fa-8x fa fa-puzzle-piece"></span>
-		<h4>Bundled with ready-to-use stuff</h4>
-		<p>from an assortment of instrument and effect plugins, presets and samples to VST and SoundFont support.</p>
-	</div>
-</div>
-<br><br><br><br>
+	<br><br><br><br>
 
-<div class="page-header">
-	<hr><h1>Made by musicians, for musicians.</h1>
-</div>
+	<div class="page-header">
+		<hr><h1>Made by musicians, for musicians.</h1>
+	</div>
 
-<div>
 	<div>
-		<a href="/download/#linux" class="btn btn-default"><span class="fa-5x fa fa-linux fa-fw"></span></a>&nbsp;
-		<a href="/download/#windows" class="btn btn-default"><span class="fa-5x fa fa-windows fa-fw"></span></a>&nbsp;
-		<a href="/download/#mac" class="btn btn-default"><span class="fa-5x fa fa-apple fa-fw"></span></a>&nbsp;
-		<h4>Take your pick</h4>
-		<p>for Linux, Windows or Apple OS X</p>
+		<div>
+			<a href="/download/#linux" class="btn btn-default"><span class="fa-5x fa fa-linux fa-fw"></span></a>&nbsp;
+			<a href="/download/#windows" class="btn btn-default"><span class="fa-5x fa fa-windows fa-fw"></span></a>&nbsp;
+			<a href="/download/#mac" class="btn btn-default"><span class="fa-5x fa fa-apple fa-fw"></span></a>&nbsp;
+			<h4>Take your pick</h4>
+			<p>for Linux, Windows or Apple OS X</p>
+		</div>
 	</div>
-</div>
 
-<br><br>
-<div class="page-header">
-	<hr><h1>Houston, we've had a problem.</h1>
-	<h4>Standby, we've got you covered.</h4>
-</div>
+	<br><br>
+	<div class="page-header">
+		<hr><h1>Houston, we've had a problem.</h1>
+		<h4>Standby, we've got you covered.</h4>
+	</div>
 
-<div>
 	<div>
-		<a href="/forum/" class="btn btn-default"><span class="fa-5x fa fa-comments fa-fw"></span></a>&nbsp;
-		<a href="http://facebook.com/makefreemusic" target="_blank" class="btn btn-default"><span class="fa-5x fa fa-facebook fa-fw"></span></a>&nbsp;
-		<a href="https://plus.google.com/u/0/113001340835122723950/posts" target="_blank" class="btn btn-default"><span class="fa-5x fa fa-google-plus fa-fw"></span></a>&nbsp;
-		<a href="http://github.com/lmms/lmms" target="_blank" class="btn btn-default"><span class="fa-5x fa fa-github fa-fw"></span></a>
-		<h4>Ask your questions here</h4>
-		<p>and answer a few questions while you're waiting.</p>
+		<div>
+			<a href="/forum/" class="btn btn-default"><span class="fa-5x fa fa-comments fa-fw"></span></a>&nbsp;
+			<a href="http://facebook.com/makefreemusic" target="_blank" class="btn btn-default"><span class="fa-5x fa fa-facebook fa-fw"></span></a>&nbsp;
+			<a href="https://plus.google.com/u/0/113001340835122723950/posts" target="_blank" class="btn btn-default"><span class="fa-5x fa fa-google-plus fa-fw"></span></a>&nbsp;
+			<a href="http://github.com/lmms/lmms" target="_blank" class="btn btn-default"><span class="fa-5x fa fa-github fa-fw"></span></a>
+			<h4>Ask your questions here</h4>
+			<p>and answer a few questions while you're waiting.</p>
+		</div>
 	</div>
-</div>
-<br>
-<p class="text-center visible-lg">View all community pages <a href="/community/">here</a>.</p>
-<br>
-<div class="page-header">
-	<hr><h1>Sharing means caring.</h1>
-</div>
+	<br>
+	<p class="text-center visible-lg">View all community pages <a href="/community/">here</a>.</p>
+	<br>
+	<div class="page-header">
+		<hr><h1>Sharing means caring.</h1>
+	</div>
 
-<div>
 	<div>
-		<a href="https://soundcloud.com/groups/linux-multimedia-studio" target="_blank" class="btn btn-default"><span class="fa-5x fa fa-soundcloud fa-fw"></span></a>&nbsp;
-		<a href="/lsp/" class="btn btn-default"><span class="fa-5x fa fa-volume-down">	LSP</span></a>&nbsp;
-		<h4>Don't be modest,</h4>
-		<p>show us your creations.</p>
+		<div>
+			<a href="https://soundcloud.com/groups/linux-multimedia-studio" target="_blank" class="btn btn-default"><span class="fa-5x fa fa-soundcloud fa-fw"></span></a>&nbsp;
+			<a href="/lsp/" class="btn btn-default"><span class="fa-5x fa fa-volume-down">	LSP</span></a>&nbsp;
+			<h4>Don't be modest,</h4>
+			<p>show us your creations.</p>
+		</div>
 	</div>
-</div>
-<br><br>
-<div class="overflow-hidden visible-lg">
-<div class="page-header">
-	<hr><h1>Take a peek</h1>
-</div>
-	<div class="col-lg-4">
-		<h4>Overall Project</h4>
-		<?php make_reflection('img/ss_proj.png', NULL, "white"); ?>
+	<br><br>
+	<div class="overflow-hidden visible-lg">
+		<div class="page-header">
+			<hr><h1>Take a peek</h1>
+		</div>
+		<div class="col-lg-4">
+			<h4>Overall Project</h4>
+			<?php make_reflection('img/ss_proj.png', NULL, "white"); ?>
+		</div>
+		<div class="col-lg-4">
+			<h4>Song Editor</h4>
+			<?php make_reflection('img/ss_song.png', NULL, "white"); ?>
+		</div>
+		<div class="col-lg-4">
+			<h4>Native Instruments</h4>
+			<?php make_reflection('img/ss_instr.png', NULL, "white"); ?>
+		</div>
 	</div>
-	<div class="col-lg-4">
-		<h4>Song Editor</h4>
-		<?php make_reflection('img/ss_song.png', NULL, "white"); ?>
-	</div>
-	<div class="col-lg-4">
-		<h4>Native Instruments</h4>
-		<?php make_reflection('img/ss_instr.png', NULL, "white"); ?>
-	</div>
-</div>
 	<p class="text-center visible-lg">More screenshots can be found <a href="/screenshots/">here</a>.</p>
-<br><br>
+	<br><br>
 </div>
 
 <div>
-<div class="page-header">
-	<hr><h1>Featuring</h1>
-</div>
+	<div class="page-header">
+		<hr><h1>Featuring</h1>
+	</div>
 	<div class="col-lg-4">
 		<h2 class="text-center"><span class="fa-3x fa fa-check-square-o"></span><br>Core Functionality</h2>
 		<ul>
-		<li>Compose music on Windows, Linux and Apple OS&nbsp;X</li>
-		<li>Sequence, compose, mix and automate songs in one simple interface</li>
-		<li>Note playback via MIDI or typing keyboard</li>
-		<li>Consolidate instrument tracks using Beat+Bassline Editor</li>
-		<li>Fine tune patterns, notes, chords and melodies using Piano Roll Editor</li>
-		<li>Full user-defined track-based automation and computer-controlled automation sources</li>
-		<li>Import of MIDI files, Hydrogen project files and FL Studio ® project files</li>
+			<li>Compose music on Windows, Linux and Apple OS&nbsp;X</li>
+			<li>Sequence, compose, mix and automate songs in one simple interface</li>
+			<li>Note playback via MIDI or typing keyboard</li>
+			<li>Consolidate instrument tracks using Beat+Bassline Editor</li>
+			<li>Fine tune patterns, notes, chords and melodies using Piano Roll Editor</li>
+			<li>Full user-defined track-based automation and computer-controlled automation sources</li>
+			<li>Import of MIDI files, Hydrogen project files and FL Studio ® project files</li>
 		</ul>
 	</div>
 	<div class="col-lg-4">
 		<h2 class="text-center"><span class="fa-3x fa fa fa-bar-chart-o"></span><br>Effects Mixing</h2>
 		<ul>
-		<li>Drop-in LADSPA plug-in support</li>
-		<li>Drop-in VST ® effect plug-in support on (Linux and Windows)</li>
-		<li>Built-in compressor, limiter, delay, reverb, distortion, EQ, bass-enhancer</li>
-		<li>Bundled graphic and parametric equalizers</li>
-		<li>Built-in visualization/spectrum analyser</li>
+			<li>Drop-in LADSPA plug-in support</li>
+			<li>Drop-in VST ® effect plug-in support on (Linux and Windows)</li>
+			<li>Built-in compressor, limiter, delay, reverb, distortion, EQ, bass-enhancer</li>
+			<li>Bundled graphic and parametric equalizers</li>
+			<li>Built-in visualization/spectrum analyser</li>
 		</ul>
 	</div>
 	<div class="col-lg-4">
 		<h2 class="text-center"><span class="fa-3x fa fa fa-music"></span><br>Musical Instruments</h2>
 		<ul>
-		<li>Built-in 32-bit VST instrument support</li>
-		<li>Built-in 64-bit VST instrument support with 32-bit VST bridge (64-bit Windows)</li>
-		<li>Roland ® TB-303 style monophonic bass synthesizer</li>
-		<li>Embedded ZynAddSubFx:  Polyphonic, mutlitimbral, microtonal, multi-voice additive, subtractive and pad synthesis all in one powerful plugin</li>
-		<li>Native Commodore 64 ® SID microchip/instrument emulation</li>
-		<li>Native SoundFont ® support (SF2), the industry standard for high quality instrument patches and banks</li>
-		<li>Nintendo ®, GameBoy ® and game sound effect emulation</li>
-		<li>2 built-in oscillator-based synthesizers</li>
-		<li>2 built-in wavetable-based synthesizers</li>
-		<li>Gravis UltraSound ® GUS Patch support</li>
+			<li>Built-in 32-bit VST instrument support</li>
+			<li>Built-in 64-bit VST instrument support with 32-bit VST bridge (64-bit Windows)</li>
+			<li>Roland ® TB-303 style monophonic bass synthesizer</li>
+			<li>Embedded ZynAddSubFx:  Polyphonic, mutlitimbral, microtonal, multi-voice additive, subtractive and pad synthesis all in one powerful plugin</li>
+			<li>Native Commodore 64 ® SID microchip/instrument emulation</li>
+			<li>Native SoundFont ® support (SF2), the industry standard for high quality instrument patches and banks</li>
+			<li>Nintendo ®, GameBoy ® and game sound effect emulation</li>
+			<li>2 built-in oscillator-based synthesizers</li>
+			<li>2 built-in wavetable-based synthesizers</li>
+			<li>Gravis UltraSound ® GUS Patch support</li>
 		</ul>
 	</div>
 </div>
