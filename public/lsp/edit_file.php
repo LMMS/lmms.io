f089685e69f3fa590d56d028555843ee161f4f4b
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-10-14 01:07:44 -0400

    Rewrite edit file page

--- public/lsp/edit_file.php
+++ public/lsp/edit_file.php
@@ -1,49 +1,55 @@
 <?php
 require_once('utils.php');
-// This line was yanked from index.php
-if (get_user_id(SESSION()) == get_object_by_id("files", GET('file'), 'user_id')) {
-//if( isset( $_SESSION["remote_user"] ) )
-//{
+require_once('dbo.php');
+global $LSP_URL;
 
+if (get_user_id(SESSION()) == get_object_by_id("files", GET('file'), 'user_id') || is_admin(get_user_id(SESSION()))) {
 	if(!POST('updateok', false)) {
-		$filename = get_file_name(GET('file'));
-		$firstdot = strpos($filename, '.'); 
-		$fext = substr($filename, $firstdot, strlen($filename) - $firstdot);
-		$cats = get_categories_for_ext($fext, get_file_category(GET('file')) . '-' . get_file_subcategory(GET('file')));
-		echo '<h1>Edit ' . $filename . '</h1>';
-		$form = new form($LSP_URL . '?content=update&file=' . GET('file'));
-		echo '<table style="border:none;" cellpadding="5">';
-		echo '<tr><td>Category:</td><td><select name="category" />';
-		echo $cats;
-		echo "</select></td></tr>\n";
-		echo '<tr><td>License:</td><td><select name="license" />';
-		echo get_licenses(get_license_name(get_file_license(GET('file'))));
-		echo "</select></td></tr></table>";
-			 
-		echo '<br />Description: <br /><textarea cols="50" rows="20" name="description">';
-		echo get_file_description(GET('file'));
-		echo "</textarea><br /><br />";
-
-		echo '<input type="submit" name="updateok" value="OK" />';
-		echo '<input type="hidden" name="fn" value="'.$filename.'" />';
-		echo '<input type="hidden" name="action" value="update" />';
-		$form->close ();
+		$file_name = get_file_name(GET('file'));
+		$file_extension = '.' . pathinfo($file_name, PATHINFO_EXTENSION);
+		$categories = get_categories_for_ext($file_extension, get_file_category(GET('file')) . ' - ' . get_file_subcategory(GET('file')));
+		echo '<div class="col-md-9">';
+		create_title(array('Edit', get_file_url()));
+		$form = new form($LSP_URL . '?content=update&file=' . GET('file'), $title = 'Edit File', 'fa-pencil'); ?>
+		<div class="form-group">
+			<label for="category">Category</label>
+			<select name="category" class="form-control">
+			<?php echo $categories; ?>	
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="license">License</label>
+			<select name="license" class="form-control">
+			<?php echo get_licenses(get_license_name(get_file_license(GET('file')))); ?>
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="description">Description</label>
+			<textarea rows=20 name="description" class="form-control"><?php
+				echo get_file_description(GET('file'));
+			?></textarea>
+		</div>
+		<button class="btn btn-primary" type="submit" name="updateok" value="OK">Update File</button>
+		<a href="<?php echo "$LSP_URL?action=show&file=" . GET('file'); ?>" class="btn btn-warning"><span class="fa fa-close"></span>&nbsp;Cancel</a>
+		<input type="hidden" name="fn" value="'.$file_name.'" />
+		<input type="hidden" name="action" value="update" />
+		<?php $form->close(); echo '</div>';
 	}  else {
-		$cat = explode('-', POST('category'));
+		$cat = explode(' - ', POST('category'));
 		$catid = get_category_id($cat[0]);
 		$subcatid = get_subcategory_id($cat[1]);
 		$licenseid = get_license_id(POST('license'));
 
+		$success = false;
 		if (update_file(GET('file'), $catid, $subcatid, $licenseid, POST('description'))) {
-			echo "<br /><span style=\"font-weight:bold; color:#0a0;\">Your changes were saved.</span> <br /><br />";
-		} else {
-			echo "<br /><span style=\"font-weight:bold; color:#f80;\">For some reasons your changes could not be saved. Please try again</span> <br /><br />";
+			$success = true;
 		}
-		show_file(GET('file'), SESSION());
+		
+		show_file(GET('file'), SESSION(), $success);
 	}
 }
 else {
-	display_error('Not logged in', array('Edit', get_file_url()));
+	display_error('Sorry, you cannot edit this file.', array('Edit', get_file_url()));
 }
 
 
