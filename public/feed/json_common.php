2265d8af950faa15825471f32f16e16d756338e4
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-10-20 23:54:05 -0400

    Follow JSON redirects for safe_mode enabled servers

--- public/feed/json_common.php
+++ public/feed/json_common.php
@@ -250,7 +250,7 @@ function file_get_contents_curl($url, $service) {
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	}
 
-	$data = curl_exec($ch);
+	$data = curl_exec_follow($ch);
 	curl_close($ch);
 
 	return $data;
@@ -461,6 +461,64 @@ function scale_image($url, $width) {
 	return 'data:image/png;base64,' . base64_encode($i). '"';
 }
 
+/*
+ * Work-around for safe_mode server configs that prevent following
+ * redirects
+ * Author: slopjong, 2012-03-31
+ */
+function curl_exec_follow($ch, &$maxredirect = 5) {
+	curl_setopt($ch, CURLOPT_USERAGENT, 'curl/7.x (linux)');
+	$mr = intval($maxredirect);
+	
+	if (filter_var(ini_get(‘open_basedir’), FILTER_VALIDATE_BOOLEAN) === false 
+      && filter_var(ini_get(‘safe_mode’), FILTER_VALIDATE_BOOLEAN) === false) {
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	} else {
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+		if ($mr > 0) {
+			$original_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+			$newurl = $original_url;
+			$rch = curl_copy_handle($ch);
 
+			curl_setopt($rch, CURLOPT_HEADER, true);
+			curl_setopt($rch, CURLOPT_NOBODY, true);
+			curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
+			do {
+				curl_setopt($rch, CURLOPT_URL, $newurl);
+				$header = curl_exec($rch);
+				if (curl_errno($rch)) {
+					$code = 0;
+				} else {
+					$code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
+					if ($code == 301 || $code == 302) {
+						preg_match('/Location:(.*?)\n/i', $header, $matches);
+						$newurl = trim(array_pop($matches));
+						
+						// if no scheme is present then the new url is a
+						// relative path and thus needs some extra care
+						if(!preg_match("/^https?:/i", $newurl)){
+						  $newurl = $original_url . $newurl;
+						}   
+					} else {
+						$code = 0;
+					}
+				}
+			} while ($code && --$mr);
+			curl_close($rch);
+			if (!$mr) {
+				if ($maxredirect === null) 
+					trigger_error('Too many redirects.', E_USER_WARNING);
+				else 
+					$maxredirect = 0;
+					return false;
+			}
+			curl_setopt($ch, CURLOPT_URL, $newurl);
+		}
+	}
+	return curl_exec($ch);
+}
 
 ?>
