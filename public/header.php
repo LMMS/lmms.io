05423adb80b4bbf5760a9557fd18d56502400d37
Lukas W <lukaswhl@gmail.com>
2014-10-27 18:02:55 +0100

    Move navbar code out of header into a separate file

--- public/header.php
+++ public/header.php
@@ -1,25 +1,5 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/../lib/Navbar.php');
-$navbar = new Navbar(
-	[
-		['Download', '/download/', [
-			['fa-download', 'Download LMMS', '/download/'],
-			['fa-music', 'Download Sample Packs', '/download/samples'],
-			['fa-picture-o', 'Download Artwork', '/download/artwork']]],
-		[['Screenshots', 'Screens'], '/screenshots/'],
-		['Showcase', '/showcase/'],
-		[['Documentation', 'Docs'], '/documentation/'],
-		['Community', '/community/', [
-			['fa-users', 'Community', '/community/'],
-			['fa-comments', 'Forums', '/forum/'],
-			['fa-facebook', 'Facebook', '/community/#facebook'],
-			['fa-soundcloud', 'SoundCloud', '/community/#soundcloud'],
-			['fa-google-plus','Google+', '/community/#google+'],
-			['fa-youtube', 'YouTube', '/community/#youtube'],
-			['fa-github', 'GitHub', '/community/#github']]],
-		//['Share', '/lsp/'],
-	]
-);
+require_once('navbar.php');
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
