a0e3a99e9cbc4433ad7af1ef95b1dc7eb6bce371
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-10-28 15:26:27 -0400

    Get-Involved: Center text, Use panel headings

--- public/get-involved/index.php
+++ public/get-involved/index.php
@@ -1,5 +1,5 @@
 <?php require_once('../header.php'); ?>
-<h1 class="page-header">Get Involved</h1>
+<h1 class="page-header text-center">Get Involved</h1>
 <p>LMMS is a free open-source software built entirely by a small number of
 volunteers in their free time. But to keep the project going, we need this
 number to grow. So if you feel like giving something back to the community,
@@ -10,7 +10,9 @@ join us and help making LMMS better!</p>
 <div class="row">
 	<div class="col-lg-6">
 		<div class="panel panel-default">
-			<div class="panel-heading" id="development"><i class="fa fa-fw fa-file-code-o"></i> Join Development</div>
+			<div class="panel-heading" id="development">
+			<h3 class="panel-title"><i class="fa fa-fw fa-file-code-o"></i> Join Development</h3>
+			</div>
 			<div class="panel-body">
 				<p>We are in desperate need of new developers joining our team.
 				LMMS is written in C++ using the Qt Framework, so if you know a thing or two about these, don't hesitate to get yourself dirty with coding!</p>
@@ -21,7 +23,8 @@ join us and help making LMMS better!</p>
 
 	<div class="col-lg-6">
 		<div class="panel panel-default">
-			<div class="panel-heading" id="docs"><i class="fa fa-fw fa-book"></i> Website & Docs</div>
+			<div class="panel-heading" id="docs">
+			<h3 class="panel-title"><i class="fa fa-fw fa-book"></i> Website & Docs</div></h3>
 			<div class="panel-body">
 				<p>You can help us by improving this website or documenting the software. Please post to the mailing list if you're interested in helping!</p>
 				<p>The website sources are in our GitHub repo <a href="//github.com/LMMS/lmms.io">LMMS/lmmms.io</a>, our documentation is hosted using a <a href="//lmms.sf.net/wiki">MediaWiki instance</a>.</p>
@@ -32,7 +35,8 @@ join us and help making LMMS better!</p>
 <div class="row">
 	<div class="col-lg-6">
 		<div class="panel panel-default">
-			<div class="panel-heading" id="donate"><i class="fa fa-fw fa-money"></i> Donate</div>
+			<div class="panel-heading" id="donate">
+			<h3 class="panel-title"><i class="fa fa-fw fa-money"></i> Donate</div></h3>
 			<div class="panel-body">
 				<p>Even though donations are not the thing we need the most (that'd be developers), they help us pay our bills, for our domain lmms.io for instance. </p>
 				<a href="https://www.paypal.com/cgi-bin/webscr?item_name=Donation+to+LMMS&cmd=_donations&business=donations%40lmms.io">
@@ -44,7 +48,8 @@ join us and help making LMMS better!</p>
 
 	<div class="col-lg-6">
 		<div class="panel panel-default">
-			<div class="panel-heading" id="translate"><i class="fa fa-fw fa-flag"></i> Translations</div>
+			<div class="panel-heading" id="translate">
+			<h3 class="panel-title"><i class="fa fa-fw fa-flag"></i> Translations</div></h3>
 			<div class="panel-body">
 				<p>If you're language is missing in LMMS, help the project and other users speaking the same language by translating the software. You can find instructions on the page "<a href="//github.com/LMMS/lmms/wiki/Creating-a-localization">Creating a localization</a>" in our GitHub Wiki.
 			</div>
