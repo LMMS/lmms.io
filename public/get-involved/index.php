e9d0075c27576e6331a3d69e0036d3eb727e3e18
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-10-28 15:48:36 -0400

    Fix typo, add links to mailing list.

--- public/get-involved/index.php
+++ public/get-involved/index.php
@@ -17,6 +17,7 @@ join us and help making LMMS better!</p>
 				<p>We are in desperate need of new developers joining our team.
 				LMMS is written in C++ using the Qt Framework, so if you know a thing or two about these, don't hesitate to get yourself dirty with coding!</p>
 				<p>See our <a href="//github.com/LMMS/lmms/wiki">GitHub Wiki</a> and our <a href="//github.com/LMMS/lmms/issues">bug tracker</a> to get started.</p>
+				<p>Furthermore, the LMMS <a href="https://lists.sourceforge.net/lists/listinfo/lmms-devel">mailing list</a> is a great place to introduce yourself.</p>
 			</div>
 		</div>
 	</div>
@@ -26,7 +27,7 @@ join us and help making LMMS better!</p>
 			<div class="panel-heading" id="docs">
 			<h3 class="panel-title"><i class="fa fa-fw fa-book"></i> Website & Docs</div></h3>
 			<div class="panel-body">
-				<p>You can help us by improving this website or documenting the software. Please post to the mailing list if you're interested in helping!</p>
+				<p>You can help us by improving this website or documenting the software. Please post to the <a href="https://lists.sourceforge.net/lists/listinfo/lmms-devel">mailing list</a> if you're interested in helping!</p>
 				<p>The website sources are in our GitHub repo <a href="//github.com/LMMS/lmms.io">LMMS/lmmms.io</a>, our documentation is hosted using a <a href="//lmms.sf.net/wiki">MediaWiki instance</a>.</p>
 			</div>
 		</div>
@@ -51,7 +52,7 @@ join us and help making LMMS better!</p>
 			<div class="panel-heading" id="translate">
 			<h3 class="panel-title"><i class="fa fa-fw fa-flag"></i> Translations</div></h3>
 			<div class="panel-body">
-				<p>If you're language is missing in LMMS, help the project and other users speaking the same language by translating the software. You can find instructions on the page "<a href="//github.com/LMMS/lmms/wiki/Creating-a-localization">Creating a localization</a>" in our GitHub Wiki.
+				<p>If your language is missing in LMMS, help the project and other users speaking the same language by translating the software. You can find instructions on the page "<a href="//github.com/LMMS/lmms/wiki/Creating-a-localization">Creating a localization</a>" in our GitHub Wiki.
 			</div>
 		</div>
 	</div>
