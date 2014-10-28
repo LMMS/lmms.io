8fba2fda76cf696bfde755f5774abe6358bafc24
Lukas W <lukaswhl@gmail.com>
2014-10-29 00:01:43 +0100

    Add "Get in touch" section

--- public/get-involved/index.php
+++ public/get-involved/index.php
@@ -1,21 +1,23 @@
 <?php require_once('../header.php'); ?>
 <h1 class="page-header text-center">Get Involved</h1>
-<p>LMMS is a free open-source software built entirely by a small number of
-volunteers in their free time. But to keep the project going, we need this
-number to grow. So if you feel like giving something back to the community,
-join us and help making LMMS better!</p>
+<p>
+	LMMS is a free open-source software built entirely by a small number of
+	volunteers in their free time. But to keep the project going, we need this
+	number to grow. So if you feel like giving something back to the community,
+	join us and help making LMMS better!
+</p>
 
+<h2>What you can do</h2>
 <p>There are multiple ways of contributing:</p>
 
 <div class="row">
 	<div class="col-lg-6">
 		<div class="panel panel-default">
 			<div class="panel-heading" id="development">
-			<h3 class="panel-title"><i class="fa fa-fw fa-file-code-o"></i> Join Development</h3>
+				<h3 class="panel-title"><i class="fa fa-fw fa-file-code-o"></i> Join Development</h3>
 			</div>
 			<div class="panel-body">
-				<p>We are in desperate need of new developers joining our team.
-				LMMS is written in C++ using the Qt Framework, so if you know a thing or two about these, don't hesitate to get yourself dirty with coding!</p>
+				<p>We are in desperate need of new developers joining our team. LMMS is written in C++ using the Qt Framework, so if you know a thing or two about these, don't hesitate to get yourself dirtywith coding!</p>
 				<p>See our <a href="//github.com/LMMS/lmms/wiki">GitHub Wiki</a> and our <a href="//github.com/LMMS/lmms/issues">bug tracker</a> to get started.</p>
 				<p>Furthermore, the LMMS <a href="https://lists.sourceforge.net/lists/listinfo/lmms-devel">mailing list</a> is a great place to introduce yourself.</p>
 			</div>
@@ -25,7 +27,8 @@ join us and help making LMMS better!</p>
 	<div class="col-lg-6">
 		<div class="panel panel-default">
 			<div class="panel-heading" id="docs">
-			<h3 class="panel-title"><i class="fa fa-fw fa-book"></i> Website & Docs</div></h3>
+				<h3 class="panel-title"><i class="fa fa-fw fa-book"></i> Website & Docs</h3>
+			</div>
 			<div class="panel-body">
 				<p>You can help us by improving this website or documenting the software. Please post to the <a href="https://lists.sourceforge.net/lists/listinfo/lmms-devel">mailing list</a> if you're interested in helping!</p>
 				<p>The website sources are in our GitHub repo <a href="//github.com/LMMS/lmms.io">LMMS/lmmms.io</a>, our documentation is hosted using a <a href="//lmms.sf.net/wiki">MediaWiki instance</a>.</p>
@@ -33,11 +36,13 @@ join us and help making LMMS better!</p>
 		</div>
 	</div>
 </div>
+
 <div class="row">
 	<div class="col-lg-6">
 		<div class="panel panel-default">
 			<div class="panel-heading" id="donate">
-			<h3 class="panel-title"><i class="fa fa-fw fa-money"></i> Donate</div></h3>
+				<h3 class="panel-title"><i class="fa fa-fw fa-money"></i> Donate</h3>
+			</div>
 			<div class="panel-body">
 				<p>Even though donations are not the thing we need the most (that'd be developers), they help us pay our bills, for our domain lmms.io for instance. </p>
 				<a href="https://www.paypal.com/cgi-bin/webscr?item_name=Donation+to+LMMS&cmd=_donations&business=donations%40lmms.io">
@@ -50,12 +55,68 @@ join us and help making LMMS better!</p>
 	<div class="col-lg-6">
 		<div class="panel panel-default">
 			<div class="panel-heading" id="translate">
-			<h3 class="panel-title"><i class="fa fa-fw fa-flag"></i> Translations</div></h3>
+				<h3 class="panel-title"><i class="fa fa-fw fa-flag"></i> Translations</h3>
+			</div>
 			<div class="panel-body">
-				<p>If your language is missing in LMMS, help the project and other users speaking the same language by translating the software. You can find instructions on the page "<a href="//github.com/LMMS/lmms/wiki/Creating-a-localization">Creating a localization</a>" in our GitHub Wiki.
+				<p>If your language is missing in LMMS, help the project and other users speaking the same language by translating the software. You can find instructions on the page "<a href="//github.com/LMMS/lmms/wiki/Creating-a-localization">Creating a localization</a>" in our GitHub Wiki.</p>
 			</div>
 		</div>
 	</div>
 </div>
+
+<h2>How to get in touch</h2>
+
+<div class="row">
+	<div class="col-lg-6">
+		<div class="panel panel-default">
+			<div class="panel-heading" id="mailinglist">
+				<h3 class="panel-title"><i class="fa fa-fw fa-envelope"></i> Mailing List</h3>
+			</div>
+			<div class="panel-body">
+				<p>Posting to the mailing list is the best way of reaching out to developers: </p>
+				<a href="http://lists.sourceforge.net/lists/listinfo/lmms-devel">Subscribe to LMMS-Devel mailing list</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-6">
+		<div class="panel panel-default">
+			<div class="panel-heading" id="irc">
+				<h3 class="panel-title"><i class="fa fa-fw fa-comments"></i> IRC</h3>
+			</div>
+			<div class="panel-body">
+				<p>Our IRC channel is <a href="irc://chat.freenode.net/#lmms">#lmms</a> on <a href="http://freenode.net/irc_servers.shtml">Freenode</a></p>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<div class="row">
+	<div class="col-lg-6">
+		<div class="panel panel-default">
+			<div class="panel-heading" id="forum">
+				<h3 class="panel-title"><i class="fa fa-fw fa-bars"></i> Forum</h3>
+			</div>
+			<div class="panel-body">
+				<p>We host a forum for our users here on this website:</p>
+				<a href="/forum">https://lmms.io/forum</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-6">
+		<div class="panel panel-default">
+			<div class="panel-heading" id="github">
+				<h3 class="panel-title"><i class="fa fa-fw fa-github"></i> GitHub</h3>
+			</div>
+			<div class="panel-body">
+				<p>For reporting a bug or suggesting a new feature, please use the GitHub issue tracker:
+				<a href="//github.com/LMMS/lmms/issues">https://github.com/LMMS/lmms/issues</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+
 
 <?php require_once('../footer.php'); ?>
