1937051eb362bb832c73ce95111685c0ba000460
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-10-29 14:58:05 -0400

    LSP file delete fix
    Somehow this was never tested.  Fixed now. :)

--- public/lsp/delete_file.php
+++ public/lsp/delete_file.php
@@ -7,7 +7,11 @@ global $LSP_URL;
 if (!SESSION_EMPTY() && 
 	(get_user_id(SESSION()) == get_file_owner(GET('file')) || is_admin(get_user_id(SESSION())))) {
 	if (GET('confirmation') == "true" ) {
-		display_success('File deleted successfully', array('Delete'));
+		if (delete_file(GET('file'))) {
+			display_success('File deleted successfully', array('Delete'));
+		} else {
+			display_error('Sorry, file ' . GET('file') . ' could not be deleted', array('Delete'));
+		}
 		get_latest();
 	} else {
 		display_warning('This will delete all comments and ratings.', array('Delete', get_file_url()));
