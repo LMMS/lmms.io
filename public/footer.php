0bdc8d5aa4685ac3de25ca5293b2f18a2ca00c04
Lukas W <lukaswhl@gmail.com>
2014-10-29 16:02:11 +0100

    Fix footer layout again

--- public/footer.php
+++ public/footer.php
@@ -2,39 +2,41 @@
 </div><br>
 <footer id="footer">
 	<div class="container">
-		<div class="row">
-			<p><div class="col-sm-3">
+		<div>
+			<span>
 				<p>LMMS</p>
 				<small>
 					<a href="/download/">Download</a><br>
 					<a href="//github.com/LMMS/lmms">Source Code</a><br>
 					<a href="/documentation">Documentation</a><br>
 				</small>
-			</div></p>
-			<p><div class="col-sm-3">
+			</span>
+			<span>
 				<p>Get Involved</p>
 				<small>
 					<a href="/get-involved/#donate">Donations</a><br>
 					<a href="/get-involved/#development">Software</a><br>
 					<a href="/get-involved/#docs">Website & Docs</a>
 				</small>
-			</div></p>
-			<p><div class="col-sm-3">
+			</span>
+		</div>
+		<div>
+			<span>
 				<p>Community</p>
 				<small>
 					<a href="/get-involved/#forum">Forum</a><br>
 					<a href="/lsp">LMMS Sharing Platform</a><br>
 					<a href="/community">Social Networks</a><br>
 				</small>
-			</div></p>
-			<p><div class="col-sm-3">
+			</span>
+			<span>
 				<p>LMMS.io</p>
 				<small>
 					<a href="/get-involved/#contact">Contact</a><br>
 					<a href="/get-involved/#github">Report a bug</a><br>
 					<a href="/download/artwork">Logo & Artwork</a><br>
 				</small>
-			</div></p>
+			</span>
 		</div>
 	</div><br>
 	<div class="text-center small">
