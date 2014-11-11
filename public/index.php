a5e489223c04a6278639de69c35fac2775188a56
Lukas W <lukaswhl@gmail.com>
2014-11-11 11:13:27 +0100

    More comments

--- public/index.php
+++ public/index.php
@@ -2,7 +2,8 @@
 
 $uri = $_SERVER['REQUEST_URI'];
 
-$patterns = [
+// Each item like: 'Page title' => [ 'URL (opt. regex)', 'page php file' ]
+$pages = [
 	'Home' => ['/', 'home.php'],
 	'Documentation' => ['/documentation(/.*)?', 'documentation.php'],
 	'Get Involved' => ['/get-involved', 'get-involved.php'],
@@ -12,19 +13,25 @@ $patterns = [
 	'Download' => ['/download', 'download.php']
 ];
 
-foreach ($patterns as $key => $value) {
-	$regex = $value[0];
-	$file = $value[1];
+// Loop all pages to find the one requested
+foreach ($pages as $key => $page) {
+	$regex = $page[0];
+	$file = $page[1];
 
+	// Escape all '/' characters.
 	$regex = str_replace('/', '\/', $regex);
+	// Add an optional '/' at the end of the URL; Ignore case
 	$regex = '/^' . $regex . '(\/)?$/i';
 
 	if (preg_match($regex, $uri)) {
+		// If this is the requested page, set the global pagetitle to be used by the navbar
 		$GLOBALS['pagetitle'] = $key;
 
+		// Include the page's php file and exit
 		require_once($file);
 		exit();
 	}
 }
 
+// If no page is found, load 404
 require_once('404.php');
