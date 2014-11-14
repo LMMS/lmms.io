1fa6b7e71639a6fbed3bba624637bc55cd42b6ce
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-11-13 20:36:52 -0500

    Allow loading of iframe via https

--- public/showcase/index.php
+++ public/showcase/index.php
@@ -6,7 +6,7 @@
 <div id="wait-div" class="text-center">
 	<img src='/img/loading.gif' /><br>
 </div>
-<iframe class="bandcamp-iframe" src="http://bandcamp.com/EmbeddedPlayer/album=2796846066/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="http://lmmsartists.bandcamp.com/album/the-best-of-lmms-vol-1">The Best of LMMS Vol.1 by LMMS Artists</a></iframe>
+<iframe class="bandcamp-iframe" src="//bandcamp.com/EmbeddedPlayer/album=2796846066/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="http://lmmsartists.bandcamp.com/album/the-best-of-lmms-vol-1">The Best of LMMS Vol.1 by LMMS Artists</a></iframe>
 </div>
 
 <?php include('../footer.php'); ?>
