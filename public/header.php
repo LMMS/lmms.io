f6871b798027acc133471fab5cfc16121f804105
Lukas W <lukaswhl@gmail.com>
2014-11-16 22:04:10 +0100

    Fix LSP

--- public/header.php
+++ public/header.php
@@ -1,4 +1,5 @@
 <?php
+require_once('app.php');
 echo $app['twig']->render('head.twig');
 ?>
 <div class="container theme-showcase main-div" role="main">
--- public/index.php
+++ public/index.php
@@ -1,16 +1,5 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/../vendor/autoload.php');
-require_once('navbar.php');
-
-$app = new Silex\Application();
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-	'twig.path' => __DIR__.'/../templates',
-	));
-
-$app['twig']->addGlobal('navbar', $navbar);
-
-$app['debug'] = true;
-
+require_once('app.php')
 
 function twigrender($file)
 {
