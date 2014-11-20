4a0cbc3e196338032eff320f87d92eaa478fb84c
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2014-11-20 08:26:30 -0500

    Force wiki to use https
    Wiki was hard-coded to use http://.  This overrides that with the
    protocol requested from the server.
    
    Closes #107

--- public/force-https.php
+++ public/force-https.php
@@ -3,9 +3,35 @@
 // Redirects all visitors to HTTPS for live site only
 $SECURE_HOST='lmms.io';
 
-// Redirect HTTP PORT 80 traffic to use HTTPS
-if ($_SERVER['HTTP_HOST'] == $SECURE_HOST && $_SERVER['HTTPS'] !== 'on' && $_SERVER['SERVER_PORT'] == '80') {
+/*
+ * Redirect HTTP PORT 80 traffic to use HTTPS
+ */
+if (is_secure_host() && $_SERVER['HTTPS'] !== 'on' && $_SERVER['SERVER_PORT'] == '80') {
 	header("Location: https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
     die();
 }
+
+/*
+ * Returns the server protocol in the server request  "http://", "https://", etc.
+ */
+function get_protocol() {
+	// Set some sane defaults
+	$protocol = is_secure_host() ? 'https://' : 'http://';
+	
+	// Override with the value requested from the server
+	if (isset($_SERVER['SERVER_PROTOCOL']) && strpos($_SERVER['SERVER_PROTOCOL'], '/') !== false) {
+		$protocol = strtolower(explode('/', $_SERVER['SERVER_PROTOCOL'])[0] . '://');
+	}
+	
+	return $protocol;
+}
+
+/*
+ * Returns true if the host-name is explicitly defined as requiring HTTPS
+ */
+function is_secure_host() {
+	global $SECURE_HOST;
+	return $_SERVER['HTTP_HOST'] == $SECURE_HOST;
+}
+
 ?>
