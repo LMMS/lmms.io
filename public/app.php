392bbad25e2d8f6587ea719143b79be642e9a9a7
liushuyu <liushuyu011@gmail.com>
2016-12-27 21:05:19 -0700

    Add i18n support for the website

--- public/app.php
+++ public/app.php
@@ -1,14 +1,17 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/../vendor/autoload.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'./../vendor/autoload.php');
 require_once('navbar.php');
+require_once('i18n.php');
 
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-	'twig.path' => __DIR__.'/../templates',
-	));
+    'twig.path' => __DIR__.'/../templates',
+    ));
 
 $app['twig']->addGlobal('navbar', $navbar);
 
-require_once($_SERVER['DOCUMENT_ROOT'].'/../lib/GitHubMarkdownEngine.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'./../lib/GitHubMarkdownEngine.php');
 use Aptoma\Twig\Extension\MarkdownExtension;
+
 $app['twig']->addExtension(new MarkdownExtension(new GitHubMarkdownEngine()));
+$app['twig']->addExtension(new Twig_Extensions_Extension_I18n());
