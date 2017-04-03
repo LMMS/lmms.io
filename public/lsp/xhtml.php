80884c1612c5545ee088c3a80d38af832a803665
Spekular <Spekular@users.noreply.github.com>
2017-04-03 21:02:44 +0200

    HTTP to HTTPS

--- public/lsp/xhtml.php
+++ public/lsp/xhtml.php
@@ -1,5 +1,5 @@
 <?php
-$DOCTYPE = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.0//EN" "http://www.w3.org/TR/xhtml-basic/xhtml-basic10.dtd">';
+$DOCTYPE = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.0//EN" "https://www.w3.org/TR/xhtml-basic/xhtml-basic10.dtd">';
 
 
 
