779b6f21bd20a08188cc483acab3a7383bede7d1
liushuyu <liushuyu011@gmail.com>
2017-10-20 12:48:00 -0600

    i18n: removed redundant functions from app.php

--- public/app.php
+++ public/app.php
@@ -17,12 +17,6 @@ $app['twig']->addGlobal('locale', $i18n->getLanguage());
 require_once($_SERVER['DOCUMENT_ROOT'].'./../lib/GitHubMarkdownEngine.php');
 use Aptoma\Twig\Extension\MarkdownExtension;
 
-$i18n_date = new Twig_SimpleFilter('i18n_date', function ($date) {
-  $real_date = DateTime::createFromFormat('!F*dS#?Y', $date);
-  $timestamp = $real_date->format('U');
-  return strftime('%c', $timestamp);
-});
 
 $app['twig']->addExtension(new MarkdownExtension(new GitHubMarkdownEngine()));
 $app['twig']->addExtension(new Twig_Extensions_Extension_I18n());
-$app['twig']->addFilter($i18n_date);
