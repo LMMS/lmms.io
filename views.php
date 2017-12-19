b3d51bfc33fb2100270454252ef82fd59e6c8422
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2017-12-19 13:49:11 -0500

    Add Linux downloads (#254)

--- views.php
+++ views.php
@@ -33,17 +33,23 @@ function downloadPage()
 		$winpre = [$releases->latestWin32Asset(false), $releases->latestWin64Asset(false)];
 		$osxstable = $releases->latestOSXAssets();
 		$osxpre = $releases->latestOSXAssets(false);
+		$linstable = $releases->latestLinuxAssets();
+		$linpre = $releases->latestLinuxAssets(false);
 
-		if ($winpre[0]['created_at'] < $winstable[0]['created_at'])
+		if ($winstable && $winpre && ($winpre[0]['created_at'] < $winstable[0]['created_at']))
 			$winpre = null;
-		if ($osxpre[0]['created_at'] < $osxstable[0]['created_at'])
+		if ($osxstable && $osxpre && ($osxpre[0]['created_at'] < $osxstable[0]['created_at']))
 			$osxpre = null;
+		if ($linstable && $linpre && ($linpre[0]['created_at'] < $linstable[0]['created_at']))
+			$linpre = null;
 
 		$vars = [
 			'winstable' => $winstable,
 			'winpre' => $winpre,
 			'osxstable' => $osxstable,
-			'osxpre' => $osxpre
+			'osxpre' => $osxpre,
+			'linstable' => $linstable,
+			'linpre' => $linpre
 		];
 	} catch (Exception $e) {
 		error_log($e);
