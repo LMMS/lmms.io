ac2c0ff8dafa4071ccad388cda031f51f3ec9465
Umcaruje <uros.maravic@gmail.com>
2017-12-23 11:03:47 +0100

    Add missing header

---
+++ public/lsp/header.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../app.php');
+echo $app['twig']->render('head.twig');
+?>
+<div class="jumbotron mini">
+	<div class="container">
+		<h2>LMMS Sharing Platform</h2>
+    <h5>Share your work with the LMMS community.</h5>
+	</div>
+</div>
+<div class="container theme-showcase main-div" role="main">
