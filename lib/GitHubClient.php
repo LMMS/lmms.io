828380b84964262d531f7159c2cc7e7144e43115
liushuyu <liushuyu011@gmail.com>
2018-02-13 16:02:36 -0700

    github-api: use oauth token

--- lib/GitHubClient.php
+++ lib/GitHubClient.php
@@ -52,8 +52,7 @@ class GitHubClient extends \Github\Client
 	public function __construct(HttpClientInterface $httpClient = null)
 	{
 		parent::__construct($httpClient);
-		$client_id = get_base64_secret('GITHUB_CLIENT_ID');
-		$client_secret = get_base64_secret('GITHUB_CLIENT_SECRET');
-		$this->authenticate($client_id, $client_secret, \Github\Client::AUTH_URL_CLIENT_ID);
+		$client_secret = get_base64_secret('GITHUB_CLIENT_OAUTH');
+		$this->authenticate($client_secret, null, \Github\Client::AUTH_HTTP_TOKEN);
 	}
 }
