91474486f6ae706cbf190594ec0d2f02d6ab1b9a
liushuyu <liushuyu011@gmail.com>
2018-02-13 14:25:01 -0700

    github-api: add cache using psr-7 apcu

--- lib/GitHubClient.php
+++ lib/GitHubClient.php
@@ -4,6 +4,8 @@ namespace LMMS;
 require_once($_SERVER['DOCUMENT_ROOT'].'/../vendor/autoload.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/feed/json_common.php');
 
+use Cache\Adapter\Apcu\ApcuCachePool;
+
 class GitHubClient extends \Github\Client
 {
 	public function __construct(HttpClientInterface $httpClient = null)
@@ -11,6 +13,8 @@ class GitHubClient extends \Github\Client
 		parent::__construct($httpClient);
 		$client_id = get_base64_secret('GITHUB_CLIENT_ID');
 		$client_secret = get_base64_secret('GITHUB_CLIENT_SECRET');
+		$cache = new ApcuCachePool();
+		$this->addCache($cache);
 		$this->authenticate($client_id, $client_secret, \Github\Client::AUTH_URL_CLIENT_ID);
 	}
 }
