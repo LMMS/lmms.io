0e1f7fb2dfe2b8ab904bd045b62beba00cc9d328
liushuyu <liushuyu011@gmail.com>
2019-12-06 14:32:39 -0700

    navbar: convert more navbar php code to twig

--- public/lsp/header.php
+++ public/lsp/header.php
@@ -1,12 +1,18 @@
 <?php
+// The following hack is necessary to get Twig templates rendered
+// Hacks start
 require_once('../../vendor/autoload.php');
 require_once('../../src/TopNav.php');
 $loader = new \Twig\Loader\FilesystemLoader('../../templates/');
 $twig = new \Twig\Environment($loader, [
-    'cache' => $_SERVER["DOCUMENT_ROOT"] . '/../tmp/',
+    'cache' => $_SERVER["DOCUMENT_ROOT"] . '/../var/cache/lsp',
 ]);
 $nav = new App\TopNav();
+$mock_app = ["request" => ["pathinfo" => $_SERVER["REQUEST_URI"]]];
 $twig->addGlobal('navbar', $nav);
+$twig->addGlobal('app', $mock_app);
+$twig->addExtension(new Twig_Extensions_Extension_I18n());
+// Hacks end
 echo $twig->render('head.twig');
 ?>
 <div class="jumbotron mini">
