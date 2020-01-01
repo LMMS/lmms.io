a12168336e8346b88ef1cb15b4b91a95b8b9c2ee
liushuyu <liushuyu011@gmail.com>
2019-12-31 19:31:42 -0700

    lsp: migrate lsp to use twig templates

--- public/lsp/polyfill.php
+++ public/lsp/polyfill.php
@@ -7,7 +7,7 @@ use Symfony\Component\Translation\Translator;
 global $twig;
 $loader = new \Twig\Loader\FilesystemLoader('../../templates/');
 $twig = new \Twig\Environment($loader, [
-    'cache' => $_SERVER["DOCUMENT_ROOT"] . '/../var/cache/lsp',
+    // 'cache' => $_SERVER["DOCUMENT_ROOT"] . '/../var/cache/lsp',
 ]);
 $tr = new Translator("en");
 $nav = new App\TopNav($tr);
@@ -15,4 +15,30 @@ $mock_app = ["request" => ["pathinfo" => $_SERVER["REQUEST_URI"]]];
 $twig->addGlobal('navbar', $nav);
 $twig->addGlobal('app', $mock_app);
 $twig->addExtension(new Twig_Extensions_Extension_I18n());
+
+function twig_render(string $template, array $params) {
+    global $twig;
+    // Determine a successful login
+    $auth_failure = false;
+    switch (GET('action')) {
+        case 'logout' : logout(); break;
+        case 'login' : 
+            if (!login()) {
+                $auth_failure = true;
+            }
+            break;
+    }
+    $common = [
+        'category_list' => get_categories(),
+        'username' => SESSION(),
+        'is_admin' => is_admin(get_user_id(SESSION())),
+        'auth_failure' => $auth_failure,
+        'commentsearch' => GET('commentsearch', false) ? 'checked' : '',
+        'sort' => GET('sort', 'date'),
+        'category' => GET('category'),
+        'subcategory' => GET('subcategory')
+    ];
+    $merged_params = array_merge($params, $common);
+    return $twig->render($template, $merged_params);
+}
 // Hacks end
--- public/lsp/user_settings.php
+++ public/lsp/user_settings.php
@@ -1,7 +1,7 @@
 <?php
 require_once('utils.php');
 require_once('dbo.php');
-require_once('xhtml.php');
+require_once('polyfill.php');
 global $LSP_URL;
 
 function apply_settings($password, $password2, $realname) {
@@ -17,29 +17,8 @@ function apply_settings($password, $password2, $realname) {
 }
 
 if ((POST('settings') != "apply" ) || (!apply_settings(POST('password'), POST('password2'), POST('realname')))) {
-	echo '<div class="col-md-9">';
-	create_title('<a href="">User Settings</a>');
-	$form = new form("$LSP_URL?account=settings", 'User Settings', 'fa-cog'); ?>
-	<div class="form-group">
-	<label for="username" class="text-muted">User Name:</label>
-	<input type="text" name="username" class="form-control" value="<?php echo SESSION(); ?>" disabled="disabled" />
-	<p class="help-block">User name cannot be changed</p>
-	</div>
-	<div class="form-group">
-	<label for="realname">Full Name:</label>
-	<input type="text" name="realname" class="form-control" value="<?php echo get_user_realname(SESSION()); ?>" />
-	</div>
-	<div class="form-group">
-	<label for="password">Password:</label>
-	<input type="password" name="password" maxlength="20" class="form-control"/>
-	</div>
-	<div class="form-group">
-	<label for="password2">Confirm Password:</label>
-	<input type="password" class="form-control" maxlength="20" name="password2" />
-	</div>
-	<button class="btn btn-primary" type="submit" name="settings" value="apply">
-	<span class="fas fa-check"></span>&nbsp;Apply</button>
-	<a href="<?php echo $LSP_URL; ?>" class="btn btn-warning"><span class="fas fa-times"></span>&nbsp;Cancel</a>
-	<?php $form->close(); echo '</div>';
+	echo twig_render('lsp/user_settings.twig', [
+		'realname' => get_user_realname(SESSION())
+	]);
 }
 ?>
