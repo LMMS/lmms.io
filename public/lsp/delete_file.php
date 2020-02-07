731dbf553a244123f546e8daa814ca27a7a64e93
liushuyu <liushuyu011@gmail.com>
2020-02-06 21:18:23 -0700

    delete_file: use POST instead of GET

--- public/lsp/delete_file.php
+++ public/lsp/delete_file.php
@@ -6,7 +6,7 @@ require_once('xhtml.php');
 global $LSP_URL;
 if (!SESSION_EMPTY() && 
 	(get_user_id(SESSION()) == get_file_owner(GET('file')) || is_admin(get_user_id(SESSION())))) {
-	if (GET('confirmation') == "true" ) {
+	if (POST('confirmation') == "true") {
 		if (delete_file(GET('file'))) {
 			display_success('File deleted successfully', array('Delete'));
 		} else {
@@ -16,11 +16,12 @@ if (!SESSION_EMPTY() &&
 	} else {
 		display_warning('This will delete all comments and ratings.', array('Delete', get_file_url()));
 		echo '<div class="col-md-9">';
-		$form = new form(null, 'Confirm Delete', 'fa-trash'); ?>
+		$form = new form("$LSP_URL?content=delete&file=" . GET('file'), 'Confirm Delete', 'fa-trash'); ?>
+		<input type="hidden" name="confirmation" value="true"></input>
 		<p class="lead">Confirm deletion of <strong><?php echo get_file_name(GET('file')); ?></strong>?</p>
 		<div class="form-group">
-		<a class="btn btn-danger" href="<?php echo "$LSP_URL?content=delete&confirmation=true&file=" . GET('file'); ?>">
-		<span class="fas fa-check"></span>&nbsp;Delete</a>
+		<button type="submit" class="btn btn-danger">
+		<span class="fas fa-check"></span>&nbsp;Delete</button>
 		<a class="btn btn-warning" href="<?php echo "$LSP_URL?action=show&file=" . GET('file'); ?>">
 		<span class="fas fa-times"></span>&nbsp;Cancel</a>
 		</form>
