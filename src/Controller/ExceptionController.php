313a44ce2f7844d10e263dcebcd8c30b37433551
liushuyu <liushuyu011@gmail.com>
2021-05-06 01:41:25 -0600

    meta: upgrade/migrate to Symfony 5.2

--- src/Controller/ExceptionController.php
+++ src/Controller/ExceptionController.php
@@ -12,15 +12,7 @@ class ExceptionController extends AbstractController
     public function showError(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null)
     {
         $code = $exception->getStatusCode();
-        $message = _('An unknown error occurred.');
-        if ($code === 404) {
-            $message = _('You seem to have taken a wrong turn.');
-        }
-        if ($code < 600 && $code >= 500) {
-            $message = _('Oops, that is not supposed to happen.');
-        }
         return new Response($this->renderView('errorpage.twig', [
-            'message' => $message,
             'code' => $code
         ]), $code);
     }
