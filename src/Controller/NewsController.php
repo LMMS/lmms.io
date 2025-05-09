3cbc1d05436308896b8fbb866affe1de0e1c2091
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2025-05-09 08:35:00 -0400

    More whitespace fixes

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -7,39 +7,39 @@ use Symfony\Component\HttpFoundation\Response;
 
 class NewsController extends AbstractController
 {
-    public function page(GraphQl $graphql): Response
-    {
-        try {
-            return $this->render('news/index.twig', [ 'news' => array_values($this->getHtml($graphql)) ]);
-        } catch (\Exception $e) {
-            error_log($e);
-            return $this->render('news/error.twig');
-        }
-    }
+	public function page(GraphQl $graphql): Response
+	{
+		try {
+			return $this->render('news/index.twig', [ 'news' => array_values($this->getHtml($graphql)) ]);
+		} catch (\Exception $e) {
+			error_log($e);
+			return $this->render('news/error.twig');
+		}
+	}
 
-    private function getHtml(GraphQl $graphql): array
-    {
-    	$results = $graphql->executeQuery($this->getQuery());
-    	$html = array();
+	private function getHtml(GraphQl $graphql): array
+	{
+		$results = $graphql->executeQuery($this->getQuery());
+		$html = array();
 
-    	foreach ($results['data']['repository']['discussions']['edges'] as $result) {
-    		$url = $result['node']['url'];
-    		$timestamp = new \DateTime($result['node']['createdAt']);
-    		$username = $result['node']['author']['login'];
-    		$body = $result['node']['bodyHTML'];
+		foreach ($results['data']['repository']['discussions']['edges'] as $result) {
+			$url = $result['node']['url'];
+			$timestamp = new \DateTime($result['node']['createdAt']);
+			$username = $result['node']['author']['login'];
+			$body = $result['node']['bodyHTML'];
 
 			# Create link to Discussions thread
 			$ts_formatted = $timestamp->format("F j, Y, g:i a");
 			$ts_linked = "<a href='$url'>$ts_formatted</a>";
 
-    		# Create page anchor
-    		$hash = $timestamp->format('Y-m-d');
-    		$pattern = '/<h1([^>]*)>(.*?)<\/h1>/';
-    		$anchor = "<a href='#$hash' id='$hash'>";
-    		$anchored = preg_replace($pattern, '<h1$1>' . $anchor . '$2</a></h1>', $body, 1);
+			# Create page anchor
+			$hash = $timestamp->format('Y-m-d');
+			$pattern = '/<h1([^>]*)>(.*?)<\/h1>/';
+			$anchor = "<a href='#$hash' id='$hash'>";
+			$anchored = preg_replace($pattern, '<h1$1>' . $anchor . '$2</a></h1>', $body, 1);
 
-    		# Append timestamped footer with Discussions link
-    		$anchored .= "<small class='text-muted'>Published $ts_linked by <a href='//github.com/$username'>@$username</a></small>";
+			# Append timestamped footer with Discussions link
+			$anchored .= "<small class='text-muted'>Published $ts_linked by <a href='//github.com/$username'>@$username</a></small>";
 
 			# Separate each post into their own <article>s
 			$anchored = "<article lang='en' class='news-post' aria-labelledby='$hash'>$anchored</article>";
@@ -51,7 +51,7 @@ class NewsController extends AbstractController
 		}
 
 		return $html;
-    }
+	}
 
 	private function getQuery(int $limit = 100): string
 	{
