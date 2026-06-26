0e9b64ff4b82b476d9b79aa09d8886b9a599f3d6
Ostap Brehin <osbre@protonmail.com>
2026-06-26 03:19:26 +0100

    Format only the news entry being displayed

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -11,71 +11,75 @@ class NewsController extends AbstractController
 	public function latest(GraphQl $graphql): Response
 	{
 		try {
-			$items = $this->getItems($graphql);
+			$nodes = $this->getNodes($graphql);
 		} catch (\Exception $e) {
 			error_log($e);
 			return $this->render('news/error.twig');
 		}
 
-		if (empty($items)) {
+		if (empty($nodes)) {
 			return new RedirectResponse('https://github.com/LMMS/lmms/discussions/categories/announcements', 302);
 		}
 
-		return new RedirectResponse($this->generateUrl('news_show', ['date' => $items[0]['date']]), 302);
+		return new RedirectResponse($this->generateUrl('news_show', ['date' => $this->dateOf($nodes[0])]), 302);
 	}
 
 	public function show(GraphQl $graphql, string $date): Response
 	{
 		try {
-			$items = $this->getItems($graphql);
+			$nodes = $this->getNodes($graphql);
 		} catch (\Exception $e) {
 			error_log($e);
 			return $this->render('news/error.twig');
 		}
 
-		$dates = array_column($items, 'date');
+		$dates = array_map(fn($n) => $this->dateOf($n), $nodes);
 		$index = array_search($date, $dates, true);
 
 		if ($index === false) {
 			throw $this->createNotFoundException();
 		}
 
+		$formatted = $this->format($nodes[$index]);
+
 		return $this->render('news/show.twig', [
-			'item' => $items[$index]['html'],
-			'title' => $items[$index]['title'],
+			'item' => $formatted['html'],
+			'title' => $formatted['title'],
 			'date' => $date,
-			'prev' => $index > 0 ? $items[$index - 1]['date'] : null,
-			'next' => $index < count($items) - 1 ? $items[$index + 1]['date'] : null,
+			'prev' => $index > 0 ? $dates[$index - 1] : null,
+			'next' => $index < count($dates) - 1 ? $dates[$index + 1] : null,
 		]);
 	}
 
-	private function getItems(GraphQl $graphql): array
+	private function getNodes(GraphQl $graphql): array
 	{
 		$results = $graphql->executeQuery($this->getQuery());
-		$items = array();
+		return array_column($results['data']['repository']['discussions']['edges'], 'node');
+	}
 
-		foreach ($results['data']['repository']['discussions']['edges'] as $result) {
-			$url = $result['node']['url'];
-			$timestamp = new \DateTime($result['node']['createdAt']);
-			$username = $result['node']['author']['login'];
-			$body = $result['node']['bodyHTML'];
+	private function dateOf(array $node): string
+	{
+		return (new \DateTime($node['createdAt']))->format('Y-m-d');
+	}
 
-			$ts_formatted = $timestamp->format("F j, Y, g:i a");
-			$ts_linked = "<a href='$url'>$ts_formatted</a>";
+	private function format(array $node): array
+	{
+		$timestamp = new \DateTime($node['createdAt']);
+		$ts_formatted = $timestamp->format("F j, Y, g:i a");
+		$ts_linked = "<a href='{$node['url']}'>$ts_formatted</a>";
 
-			$hash = $timestamp->format('Y-m-d');
-			$pattern = '/<h1([^>]*)>(.*?)<\/h1>/';
-			$title = preg_match($pattern, $body, $m) ? trim(html_entity_decode(strip_tags($m[2]))) : $ts_formatted;
-			$anchor = "<a href='#$hash' id='$hash'>";
-			$anchored = preg_replace($pattern, '<h1$1>' . $anchor . '$2</a></h1>', $body, 1);
+		$hash = $timestamp->format('Y-m-d');
+		$pattern = '/<h1([^>]*)>(.*?)<\/h1>/';
+		$body = $node['bodyHTML'];
+		$title = preg_match($pattern, $body, $m) ? trim(html_entity_decode(strip_tags($m[2]))) : $ts_formatted;
+		$anchor = "<a href='#$hash' id='$hash'>";
+		$html = preg_replace($pattern, '<h1$1>' . $anchor . '$2</a></h1>', $body, 1);
 
-			$anchored .= "<small class='text-muted'>Published $ts_linked by <a href='//github.com/$username'>@$username</a></small>";
-			$anchored = "<article lang='en' class='news-post' aria-labelledby='$hash'>$anchored</article>";
+		$username = $node['author']['login'];
+		$html .= "<small class='text-muted'>Published $ts_linked by <a href='//github.com/$username'>@$username</a></small>";
+		$html = "<article lang='en' class='news-post' aria-labelledby='$hash'>$html</article>";
 
-			$items[] = ['date' => $hash, 'html' => $anchored, 'title' => $title];
-		}
-
-		return $items;
+		return ['html' => $html, 'title' => $title];
 	}
 
 	private function getQuery(int $limit = 100): string
